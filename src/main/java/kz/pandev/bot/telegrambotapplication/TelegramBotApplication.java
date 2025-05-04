941a883d73974734d91aa4a50caf24420ee646d6
Yelshod <yelshodtoreyev@gmail.com>
2025-05-05 01:01:03 +0500

    some changes configuration

--- src/main/java/kz/pandev/bot/telegrambotapplication/TelegramBotApplication.java
+++ src/main/java/kz/pandev/bot/telegrambotapplication/TelegramBotApplication.java
@@ -1,13 +1,24 @@
 package kz.pandev.bot.telegrambotapplication;
 
+import kz.pandev.bot.telegrambotapplication.bot.CategoryBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class TelegramBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TelegramBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
 
+    @Bean
+    public TelegramBotsApi telegramBotsApi(CategoryBot bot) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(bot);
+        return botsApi;
+    }
 }
--- src/main/java/kz/pandev/bot/telegrambotapplication/config/BotConfig.java
+++ src/main/java/kz/pandev/bot/telegrambotapplication/config/BotConfig.java
@@ -1,4 +1,15 @@
 package kz.pandev.bot.telegrambotapplication.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "telegram.bot")
 public class BotConfig {
+    private String username;
+    private String token;
+
 }
